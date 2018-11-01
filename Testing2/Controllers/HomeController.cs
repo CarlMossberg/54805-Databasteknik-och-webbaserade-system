d961c0b99ecda4dfabda80defb97978d605b26d5
Calle <carlmos@kth.se>
2018-11-01 15:10:29 +0100

    s
    Second

--- Testing2/Controllers/HomeController.cs
+++ Testing2/Controllers/HomeController.cs
@@ -16,10 +16,34 @@ namespace Testing2.Controllers
         public ActionResult Index(FormCollection collection)
         {
             ViewBag.Message = "Nu är jag i den andra indexen (HttpPost)";
+            if (collection["Min första"] != null)
+            {
+                ViewBag.Message += "Du tryckte på första knappen";
+            }
+            if (collection["Min andra"] != null)
+            {
+                ViewBag.Message += "Du tryckte på andra knappen";
+            }
             ViewBag.Message += collection["Textruta"].ToString();
             return View();
         }
 
+        public ActionResult Index2()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index2(FormCollection collection)
+        {
+            Session["MittData"] = collection["Textruta"].ToString();
+            return RedirectToAction("Index3");
+        }
+        public ActionResult Index3(FormCollection collection)
+        {
+            ViewBag.Message = Session["MittData"].ToString();
+            return View();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
